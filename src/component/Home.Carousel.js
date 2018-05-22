2dcf5a54190b9bb74c2d66400dfc3bd570ede6c2
Zack Blaylock <zackblaylock@gmail.com>
2018-05-22 17:24:46 -0400

    Added react-responsive-carousel

--- src/component/Home.Carousel.js
+++ src/component/Home.Carousel.js
@@ -1,52 +1,35 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-// import {Link} from 'react-router-dom';
-import Slider from 'react-slick';
+import {Carousel} from 'react-responsive-carousel';
 import _ from 'lodash';
-import { Container, Grid, Segment, Button, Header, Item, Image, Icon } from 'semantic-ui-react';
-//import {FacebookLogin, TwitterLogin} from '../actions/action.user';
-
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  lazyLoad: true,
-  slidesToShow: 1,
-  slidesToScroll: 1
-};
 
 class HomeCarousel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
 
     }
   }
 
-  render() {
-    const hasList = !!(this.props.featured.list);
-    return hasList ? (
-      <Slider {...settings}>
-        {
-          _.map(this.props.featured.list, feature => {
-            return(
-              <div >
-                <div className="slideContent">
-                  <h1>{feature.title}</h1>
-                </div>
-                <div className="slideInner">
-                  <Container text textAlign='center'>
-                    {/*<Link>To Video View</Link>*/}
-                    {/*<Link>To user Profile</Link>*/}
-                  </Container>
-                </div>
-              </div>
-            )
-          })
+  renderContent(featuredList) {
+    return _.map(featuredList, item => {
+      return (
+        <div>
+          <img src="https://res.cloudinary.com/diygdnbei/image/upload/v1519444005/zumnvvbqi0fo1zthkal7.png" />
+          <p className="legend">{item.title}</p>
+        </div>
+      );
+    })
+  }
 
-        }
-      </Slider>
-  ):(<div>Loading...</div>)
+  render() {
+    const {featured} = this.props;
+    return (
+      <Carousel showArrows={true}>
+                {/*onChange={onChange}> onClickItem={onClickItem} onClickThumb={onClickThumb}>*/}
+        {this.renderContent(featured)}
+      </Carousel>
+    );
   }
 }
 
